bdb6b36afaa6ee79a1a0f39495e0e7b6ebb9cd2b
josejuansorribes <josejuansorribes@users.noreply.github.com>
2019-08-01 19:25:21 +0200

    cleaned comments

--- PhotoFrame/auth.js
+++ PhotoFrame/auth.js
@@ -1,16 +1,4 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
 
 const config = require('./config.js');
 
@@ -22,25 +10,7 @@ var {google} = require('googleapis');
 const auth={};
 oAuth2Object = undefined;
 
-//ORIGINAL
-/*
-const GoogleOAuthStrategy = require('passport-google-oauth20').Strategy;
-module.exports = (passport) => {
-  passport.serializeUser((user, done) => done(null, user));
-  passport.deserializeUser((user, done) => done(null, user));
-  passport.use(new GoogleOAuthStrategy(
-  {
-    clientID: config.oAuthClientID,
-    clientSecret: config.oAuthclientSecret,
-    callbackURL: config.oAuthCallbackUrl,
-        // Set the correct profile URL that does not require any additional APIs
-        userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
-      },
-      (token, refreshToken, profile, done) => done(null, {profile, token})));
-};
-*/
 
-//TODO: ADDED
 
 module.exports.authenticate = () =>{
   // Load client secrets from a local file.
@@ -77,13 +47,7 @@ module.exports.getToken = async function(){
           //console.log("getAccessToken:", token);
           if (err) return console.error('Error retrieving access token', err);
           if(res){
-              module.exports.saveToken( oAuth2Object,res.data);/*
-                oauth2Client.setCredentials(res.data);
-                  // Store the token to disk for later program executions
-                  fs.writeFile(config.TOKEN_PATH, JSON.stringify(res.data), (err) => {
-                    if (err) return console.error(err);
-                    console.log('Token stored to', config.TOKEN_PATH);
-                  });*/
+              module.exports.saveToken( oAuth2Object,res.data);
                 }
 
                 resolve(token);
